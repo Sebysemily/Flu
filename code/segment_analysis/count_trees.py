3f21d4c2d69b80c8426eb3dedc51bf1711d87b79
Sebysemily <sebascalvas@outlook.com>
2026-08-24 12:14:15 -0500

    Clean up orphan scripts from the repository

--- code/segment_analysis/count_trees.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-tree_count = 0
-with open('results/beast/GSS/ucln_bay/H5N1_HA_panel_postQC.trees', 'r') as f:
-    for line in f:
-        if line.startswith('tree '):
-            tree_count += 1
-            
-print(f"Total trees: {tree_count}")
